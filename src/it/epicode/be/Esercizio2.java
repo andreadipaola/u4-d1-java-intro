a5d547feaaf16ec569f38873d621a6357909f85e
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-05-02 18:13:57 +0200

    added some changes

--- src/it/epicode/be/Esercizio2.java
+++ src/it/epicode/be/Esercizio2.java
@@ -5,21 +5,21 @@ public class Esercizio2 {
 	public static void main(String[] args) {
 		int x = 2;
 		int y = 3;
-		String[] array = new String[] {"Hello", "My", "Name", "is", "Andrea"};
+		String[] array = new String[] {"Hello","My","Name","is","Andrea"};
 		String word = "First";
 		int pos = 2;
-		System.out.println(Multiply(x,y));
-		System.out.println(Concat(word,x));
-		System.out.println(Arrays.toString(InsertInArray(array, word,
+		System.out.println(multiply(x,y));
+		System.out.println(concat(word,x));
+		System.out.println(Arrays.toString(insertInArray(array, word,
 	            pos)));
 	}
-	public static int Multiply(int a, int b) {
+	public static int multiply(int a, int b) {
 		return  a * b;
 	}
-	public static String Concat(String string, int a) {
+	public static String concat(String string, int a) {
 		return string + a;
 	}
-	public static String[] InsertInArray(String[] stringArray, String string, int position) {
+	public static String[] insertInArray(String[] stringArray, String string, int position) {
 		String[] resultArray = new String[stringArray.length + 1];
 		int i;
 		for(i = 0; i < resultArray.length; i++) {
@@ -33,7 +33,5 @@ public class Esercizio2 {
 			}
 		}
 		return (resultArray);
-		
 	}
-	
 }
--- src/it/epicode/be/Esercizio4.java
+++ src/it/epicode/be/Esercizio4.java
@@ -6,25 +6,19 @@ public class Esercizio4 {
 		double x = 12.5;
 		double y = 7.5;
 		double z = 9.5;
-		int w = 7;
-		System.out.println(RectanglePerimeter(x,y));
-		System.out.println(EvenAndOdd(w));
-		System.out.println(TriangleArea(x,y,z));
-		
+		int w = 6;
+		System.out.println(rectanglePerimeter(x,y));
+		System.out.println(evenAndOdd(w));
+		System.out.println(triangleArea(x,y,z));
 	}
-	public static double RectanglePerimeter(double a, double b) {
+	public static double rectanglePerimeter(double a,double b) {
 		return 2*a + 2*b;	
 	}
-	public static int EvenAndOdd(int a) {
-		if(a%2 == 0) {
-			return 0;
-		} else {
-			return 1;
-		}
+	public static int evenAndOdd(int a) {
+		return a%2 == 0 ? 0 : 1;
 	}
-	public static double TriangleArea(double a, double b, double c) {
-		double p = (a + b + c)/2;	
-		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-		
+	public static double triangleArea(double a,double b,double c) {
+		double p=(a+b+c)/2;	
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));	
 	}
 }
