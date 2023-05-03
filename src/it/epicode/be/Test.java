3392672a8431d2d2c4c454d5b62505479e3a7f34
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-05-03 16:57:42 +0200

    added some changes

--- src/it/epicode/be/Test.java
+++ src/it/epicode/be/Test.java
@@ -3,13 +3,16 @@ package it.epicode.be;
 public class Test {
 
 	public static void main(String[] args) {
-		float x = 10;
-		//Si consiglia di usare double perche si potrebbere avere problemi a causa di arrotondamenti dovuti a meccaniche interne di Java
-		float y = 1.0;
-		System.out.println(add(x,y));
-		
+		double x = 3.5;
+		// Si consiglia di usare double perche si potrebbere avere problemi a causa di
+		// arrotondamenti dovuti a meccaniche interne di Java
+		// float y = 1.0;
+		double y = 2.5;
+		System.out.println(add(x, y));
+
 	}
-	public static float add(float a, float b) {
+
+	public static double add(double a, double b) {
 		return a + b;
 	}
 
